8c09131da329e8744cc821847dcd690827c77c78
Abzal <abzal.myrzash006@gmail.com>
2022-10-15 03:47:33 +0600

    task 3

--- Task3/RandomDataPlot/script.js
+++ Task3/RandomDataPlot/script.js
@@ -5,14 +5,6 @@ function randBetween(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * The value is no lower than min (or the next integer greater than min
- * if min isn't an integer) and no greater than max (or the next integer
- * lower than max if max isn't an integer).
- * Using Math.round() will give you a non-uniform distribution!
- */
-
 function randInRange(range) {
     let min = range[0];
     let max = range[1];
